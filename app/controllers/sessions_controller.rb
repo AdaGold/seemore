ff0f7a0bb8c0d70fcc35faaee938eb2d3455665a
Jenna Nichols <jennaplusplus@gmail.com>
2016-01-06 15:20:55 -0800

    first pass at twitter oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = request.env['omniauth.auth']
+
+    # Find an identity here
+    @identity = Identity.find_with_omniauth(auth)
+
+    if @identity.nil?
+      # If no identity was found, create a brand new one here
+      @identity = Identity.create_with_omniauth(auth)
+      @user = User.create_with_omniauth(auth["info"])
+      @user.identity << @identity
+    else
+      @user = @identity.user
+    end
+
+    redirect_to root_path
+
+  end
 end
