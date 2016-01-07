6d13937b9bcb7970c45efaae435b489f4ce06a7e
Ricky Hougland <houglande@gmail.com>
2016-01-07 12:20:21 -0800

    first pass at writing how to save a handle into the handle model

--- app/models/handle.rb
+++ app/models/handle.rb
@@ -8,4 +8,17 @@ class Handle < ActiveRecord::Base
   def self.fetch_user(user)
     @user = $twitter.user(user)
   end
+
+  def self.create(hash)
+    # not sure whre hash will come from
+    # in the omniauth ex from class, auth = request.env['omniauth.auth'],
+    # which is sent from the sessions#create
+
+    handle = Handle.new
+    handle.name = hash["name"]
+    handle.description = hash["description"]
+    handle.uri = hash["uri"]
+    handle.uri_image_profile = hash["uri_image_profile"]
+    handle.save
+  end
 end
