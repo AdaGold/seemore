2fe99c502168e4dd7d70bd08f46ef54d30b82e53
Ricky Hougland <houglande@gmail.com>
2016-01-07 15:35:59 -0800

    finish tests for create medium method, and raised integer limit on twitter_id for handle model

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20160107215543) do
 
   create_table "handles", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "description"
     t.string   "uri"
     t.string   "profile_image_uri"
-    t.integer  "twitter_id"
+    t.integer  "twitter_id",        limit: 8
   end
 
   create_table "handles_users", id: false, force: :cascade do |t|
--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Medium, type: :model do
-  let(:twitter_user_instance) do
-    $twitter.user("houglande")
-  end
-
   describe ".validates" do
     context "when created with good parameters" do
       it "will be valid" do
@@ -19,22 +15,23 @@ RSpec.describe Medium, type: :model do
         expect(Medium.new(type: "Tweet", handle_id: nil, uri: "website")).to be_invalid
         expect(Medium.new(type: "Tweet", handle_id: 1, uri: nil)).to be_invalid
       end
-    #   it "will not save to database" do
-    #     Medium.create(name: nil, twitter_id: 1, uri: "link")
-    #     Medium.create(name: "example", twitter_id: nil, uri: "link")
-    #     Medium.create(name: "example", twitter_id: 1, uri: nil)
-    #     expect(Medium.all.count).to eq 0
-    #   end
+      it "will not save to database" do
+        Medium.create(type: nil, handle_id: 1, uri: "website")
+        Medium.create(type: "Tweet", handle_id: nil, uri: "website")
+        Medium.create(type: "Tweet", handle_id: 1, uri: nil)
+        expect(Medium.all.count).to eq 0
+      end
     end
   end
 
-  # describe ".create_Medium" do
-  #   context "when given an instance of Twitter::User" do
-  #     it "adds an instance of Medium" do
-  #       expect(Medium.all.count).to eq 0
-  #       Medium.create_Medium(twitter_user_instance)
-  #       expect(Medium.all.count).to eq 1
-  #     end
-  #   end
-  # end
+  describe ".create_medium" do
+    context "when given an instance of Twitter::Tweet" do
+      it "adds an instance of Medium" do
+        expect(Medium.all.count).to eq 0
+        Handle.create(name: "example", twitter_id: 7505382, uri: "link")
+        Medium.create_medium($twitter.status(27558893223))
+        expect(Medium.all.count).to eq 1
+      end
+    end
+  end
 end
