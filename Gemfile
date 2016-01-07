796caf141dd39348a10bbbbe4b52c5e45cd3c091
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-07 10:36:32 -0800

    update gemfile for vimeo oauth

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'httparty'
 gem 'omniauth'
+gem 'omniauth-oauth2', '~> 1.3.1'
 gem 'omniauth-twitter'
 gem 'omniauth-vimeo'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,19 +11,27 @@ class SessionsController < ApplicationController
       @identity = Identity.create_with_omniauth(auth)
     end
 
+    # CURRENTLY, all this is linking the identity to the user, not linking any feeds or follower handles
     if signed_in?
       if @identity.user == current_user
+        # this shouldn't happen unless we add a button to a logged in user allowing them to link the other account (link is not linking a feed, it's creating another identity for the user)
         redirect_to root_path, notice: "Already linked that account!"
       else
+        # Linking an identity with the signed in user.  Identity hasn't previously been linked with user.
+        # need to add code to stop before changing the identity's user; we want to give the user the option to merge accounts so the two identities point to the same user (one user gets deleted), but we want to make sure the user is ok and understands this first. See Daphne's drawing. 
         @identity.user = current_user
         @identity.save
         redirect_to root_path, notice: "Successfully linked that account!"
       end
     else
+      # not signed in
       if @identity.user.present?
+        # user has signed in to site before, our application can find the identity and it is already associated with a user in our database.
+        #this sets the session
         self.current_user = @identity.user
         redirect_to root_path, notice: "Signed in!"
       else
+        # user has not signed in to the site with either account (twitter or vimeo); it's a completely new user.
         @user = User.create_with_omniauth(auth["info"])
         @identity.user = @user
         @identity.save
