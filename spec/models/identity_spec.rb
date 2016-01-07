223a8d9ad6781c438d4f9ed18f39e2f9a35ecc25
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-07 12:24:37 -0800

    Add Identity model validations and rspec test

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
+  let(:twitter_identity) {
+    Identity.new(
+    uid: "1234",
+    provider: "twitter",
+    user_id: "1"
+    )
+  }
+
+  let(:vimeo_identity) {
+    Identity.new(
+    uid: "1234",
+    provider: "vimeo",
+    user_id: "1"
+    )
+  }
+
+  describe "validations" do
+    it "is valid" do
+      expect(twitter_identity).to be_valid
+      expect(vimeo_identity).to be_valid
+    end
+  end
 end
