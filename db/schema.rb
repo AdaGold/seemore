4fde372c4e7d2fcbb0950959c53e230b81c510c7
Daphne <dg1181@nyu.edu>
2016-01-06 17:06:36 -0800

    Updated index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20160106223925) do
+
+ActiveRecord::Schema.define(version: 20160106234057) do
 
   create_table "handles", force: :cascade do |t|
     t.string   "name"
