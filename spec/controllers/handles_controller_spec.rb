2a6acbb90154b87d42b8455c898ca77e5c932910
Ricky Hougland <houglande@gmail.com>
2016-01-12 10:27:16 -0800

    edit welcome controller test to add context that user is logged in

--- spec/controllers/handles_controller_spec.rb
+++ spec/controllers/handles_controller_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe HandlesController, type: :controller do
-  let(:handle) { create(:handle) }
-  let(:user1) { create(:user1) }
-
-  describe "GET 'show'" do
-    context "when logged in" do
-      it "renders the show page" do
-        session[:user_id] = user1.id
-        get :show, id: handle.id
-        expect(subject).to render_template :show
-      end
-    end
-  end
 end
