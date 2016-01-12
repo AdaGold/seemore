354a4638220b8ba383e98065725ff6356d119819
Daphne <dg1181@nyu.edu>
2016-01-11 16:05:45 -0800

    Profile_image_uri for handle added

--- lib/vimeo/user.rb
+++ lib/vimeo/user.rb
@@ -1,19 +1,24 @@
 module Vimeo
   class User < Base
     attr_reader :uri, :name, :link
-    attr_accessor :subscribed
+    attr_accessor :subscribed, :profile_image_uri
 
     def initialize(user_hash)
       @uri = user_hash["uri"]
       @name = user_hash["name"]
       @link = user_hash["link"]
+      @profile_image_uri = nil
       @subscribed = false
     end
 
     def self.find_by_uri(handle_uri)
       response = HTTParty.get("#{BASE_URI}#{handle_uri}", headers: HEADERS)
       parsed_response = JSON.parse(response)
-      self.new(parsed_response)
+      
+      new_user = self.new(parsed_response)
+      new_user.profile_image_uri = parsed_response["pictures"]["sizes"].last["link"] unless parsed_response["pictures"].nil?
+
+      return new_user
     end
 
     def self.find_by_name(user_name, page=1)
