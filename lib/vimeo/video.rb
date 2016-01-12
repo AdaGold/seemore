9d127fa7fcb345d89a452bc92eb8469d5455b6fa
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-11 17:00:34 -0800

    capture create time from videos

--- lib/vimeo/video.rb
+++ lib/vimeo/video.rb
@@ -3,16 +3,15 @@ module Vimeo
     attr_reader :uri, :name, :link, :embed
     attr_accessor :subscribed
 
-    def initialize(user_hash)
-      @uri = user_hash["uri"]
-      @name = user_hash["name"]
-      @embed = user_hash["embed"]["html"].delete('\"')
-      @description = user_hash["description"]
+    def initialize(video_hash)
+      @uri = video_hash["uri"]
+      @posted_at = video_hash["created_time"]
+      @embed = video_hash["embed"]["html"].delete('\"')
     end
 
-    def self.get_user_videos(user_uri, page=1, per_page=5)
-      #
-      query = "fields=name,description,embed,uri&page=#{page}&per_page=#{per_page}"
+    def self.get_user_videos(video_uri, page=1, per_page=5)
+      # now that we have create time, pull the first 5
+      query = "fields=created_time,embed,uri&page=#{page}&per_page=#{per_page}"
 
       response = HTTParty.get("#{BASE_URI}/#{user_uri}/videos?#{query}", headers: HEADERS)
       parsed_response = JSON.parse(response)
