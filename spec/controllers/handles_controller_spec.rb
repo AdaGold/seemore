407a06c86ad030a455e45474660aa55446c48163
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-13 10:15:47 -0800

    add tests for handle creation to handles_controller_spec

--- spec/controllers/handles_controller_spec.rb
+++ spec/controllers/handles_controller_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe HandlesController, type: :controller do
-  let(:twitter_handle) { create(:twitter_handle) }
+  # let(:vimeo_handle) { create(:vimeo_handle) }
+  let(:user1) { create(:user1) }
   describe "#subscribe" do
+    let(:twitter_params) do
+      { "provider" => "twitter", "handle_uri" => "https://twitter.com/sferik", "handle_username" => "houglande" }
+    end
+
+    let(:vimeo_params) do
+      { "provider" => "vimeo", "handle_uri" => "/users/253450" }
+    end
+
     context "when handle already exists in our database" do
-      # it "does not create a new handle object" do
-      #   handle = create(:handle)
-      #   post :subscribe, provider: "vimeo", handle_uri: handle.uri
-      #
-      # end
+
     end
 
     context "when handle does not yet exist in our database" do
       context "when provider is twitter" do
-        # post :subscribe, provider: "twitter", handle_uri: handle.uri
+        it "creates a new handle with twitter as provider and 5 media" do
+          session[:user_id] = user1.id
+          post :subscribe, twitter_params
 
+          expect(Handle.all.last.provider).to eq "twitter"
+          expect(Medium.all.length).to eq 5
+        end
       end
 
       context "when provider is vimeo" do
+        it "creates a new handle with twitter as provider and 5 media" do
+          session[:user_id] = user1.id
+          post :subscribe, vimeo_params
 
+          expect(Handle.all.last.provider).to eq "vimeo"
+          expect(Medium.all.length).to eq 5
+        end
       end
     end
 
