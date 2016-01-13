44fdaa04ecbb8d1f5295e61b920368eb0cdda5c4
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-13 11:11:59 -0800

    test for redirect_to back for handles#subscribe

--- spec/controllers/handles_controller_spec.rb
+++ spec/controllers/handles_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe HandlesController, type: :controller do
-  # let(:vimeo_handle) { create(:vimeo_handle) }
   let(:user1) { create(:user1) }
+
   describe "#subscribe" do
     let(:twitter_params) do
       { "provider" => "twitter", "handle_uri" => "https://twitter.com/sferik", "handle_username" => "houglande" }
@@ -12,38 +12,36 @@ RSpec.describe HandlesController, type: :controller do
       { "provider" => "vimeo", "handle_uri" => "/users/253450" }
     end
 
-    context "when handle already exists in our database" do
-
-    end
-
     context "when handle does not yet exist in our database" do
       context "when provider is twitter" do
-        it "creates a new handle with twitter as provider and 5 media" do
+        it "creates a new handle with twitter as provider and 5 associated media" do
           session[:user_id] = user1.id
           post :subscribe, twitter_params
-
           expect(Handle.all.last.provider).to eq "twitter"
           expect(Medium.all.length).to eq 5
+          expect(Handle.all.last.media).to eq Medium.all
         end
       end
+    end
 
-      context "when provider is vimeo" do
-        it "creates a new handle with twitter as provider and 5 media" do
-          session[:user_id] = user1.id
-          post :subscribe, vimeo_params
-
-          expect(Handle.all.last.provider).to eq "vimeo"
-          expect(Medium.all.length).to eq 5
-        end
+    context "when provider is vimeo" do
+      it "creates a new handle with vimeo as provider and 5 associated media" do
+        session[:user_id] = user1.id
+        post :subscribe, vimeo_params
+        expect(Handle.all.last.provider).to eq "vimeo"
+        expect(Medium.all.length).to eq 5
+        expect(Handle.all.last.media).to eq Medium.all
       end
     end
 
     context "when user has already subscribed to handle" do
-
-    end
-
-    context "when user has not previously subscribed to handle" do
-
+      it "redirects to last page" do
+        session[:user_id] = user1.id
+        user1.handles << create(:twitter_handle)
+        request.env["HTTP_REFERER"] = "where_i_came_from"
+        post :subscribe, twitter_params
+        expect(subject).to redirect_to "where_i_came_from"
+      end
     end
   end
 end
